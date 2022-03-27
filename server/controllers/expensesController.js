ca3bebe58f7ee9155c2d998f591efecb5310fd20
Brush87 <dan.huot87@gmail.com>
2022-03-26 23:09:56 -0600

    A bit more routing before switching to client

--- server/controllers/expensesController.js
+++ server/controllers/expensesController.js
@@ -1,4 +1,40 @@
-export const getExpenses = (req, res) => {
-  res.send('Controller logic in place')
+import Expense from '../models/expenseModel.js'
+
+export const getExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find()
+    console.log('Expenses', expenses)
+    res.status(200).json(expenses)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
 }
 
+export const createExpense = async (req, res) => {
+  const expense = req.body
+  const newExpense = new Expense(expense)
+  try {
+    await newExpense.save()
+    res.status(201).json(newExpense)
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+}
+
+// TODO Write out patchExpense Functionality
+export const patchExpense = async (req, res) => {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+// TODO Write out deleteExpense Functionality
+export const deleteExpense = async (req, res) => {
+  try {
+
+  } catch (err) {
+
+  }
+}
--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import {
-  getExpenses
+  getExpenses,
+  createExpense,
+  patchExpense,
+  deleteExpense
 } from '../controllers/expensesController.js'
 
 const router = express.Router()
 
 router.get('/', getExpenses)
+router.post('/', createExpense)
+router.patch('/', patchExpense)
+router.delete('/', deleteExpense)
 
 export default router
