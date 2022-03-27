ec050799286ff7fd2dcab6e6e092f3d63f0def78
Brush87 <dan.huot87@gmail.com>
2022-03-26 22:37:42 -0600

    Working basic route

--- server/index.js
+++ server/index.js
@@ -2,4 +2,21 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+// TIL, when using the ESM loader you need to add the file type others ERR_MODULE_NOT_FOUND
+import expenseRoutes from './routes/expenses.js'
 
+const app = express()
+
+app.use('/expense', expenseRoutes)
+
+app.use(bodyParser.json({ limit: '1mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }))
+app.use(cors())
+
+// TODO Secure Credientials at least somewhat
+const CONN_URL = 'mongodb+srv://huotdyanmicly:BKNTEcRTXcugZixr@huotdynamiclycluster.3dlzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.port || 5000
+
+mongoose.connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((err) => console.error(err.message))
